4575140a28d386dd3a3c3ef49231182b44219638
Jui Chang <juichang@Juis-MacBook-Air.local>
2019-03-16 20:51:37 +0800

    create lab1_file_transfer, tcp read write in turn, show nanosecond

---
+++ tcp/echo_once/lab1_file_transfer.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define NUM_CHUNK 20
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+
+void print_current_time_with_ms (void)
+{
+    long            ms; // Microseconds
+    time_t          s;  // Seconds
+    struct timespec spec;
+
+    clock_gettime(CLOCK_REALTIME, &spec);
+
+    s  = spec.tv_sec;
+    ms = round(spec.tv_nsec / 1.0e3); // Convert nanoseconds to microseconds
+    if (ms > 999999) {
+        s++;
+        ms = 0;
+    }
+
+    printf("Current time: %"PRIdMAX".%03ld seconds since the Epoch\n",
+           (intmax_t)s, ms);
+}
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int tcp_send(int argc, char *argv[]); // server
+int tcp_recv(int argc, char *argv[]); // client
+int udp_send(int argc, char *argv[]);
+int udp_recv(int argc, char *argv[]);
+
+int main(int argc, char *argv[]){
+    if (!strcmp(argv[1], "tcp") && !strcmp(argv[2], "send")){
+        printf("main() if : tcp_send\n");
+        tcp_send(argc, argv);
+    } else if (!strcmp(argv[1], "tcp") && !strcmp(argv[2], "recv")){
+        tcp_recv(argc, argv);
+    } else if (!strcmp(argv[1], "udp") && !strcmp(argv[2], "send")){
+        udp_send(argc, argv);
+    } else if (!strcmp(argv[1], "udp") && !strcmp(argv[2], "recv")){
+        udp_recv(argc, argv);
+    } else
+        printf("ERROR : arguments invalid\n");
+    return 0;
+}
+
+int tcp_send(int argc, char *argv[]){
+
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char confirm_buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    int chunk_size;
+    char *buffer_ptr;
+    FILE *file;
+    size_t nread;
+
+    file = fopen(argv[5], "r");
+    if (!file)
+        printf("ERROR : opening file.\n");
+    
+    size_t pos = ftell(file);    // Current position
+    fseek(file, 0, SEEK_END);    // Go to end
+    size_t length = ftell(file); // read the position which is the size
+    fseek(file, pos, SEEK_SET);  // restore original position
+    printf("length = %zu\n", length);
+    chunk_size = length / NUM_CHUNK;
+    buffer_ptr = (char *)malloc(chunk_size);
+
+    // if (file) {
+    //     while ((nread = fread(buf, 1, sizeof buf, file)) > 0)
+    //         fwrite(buf, 1, nread, stdout);
+    //     if (ferror(file)) {
+    //         /* deal with error */
+    //     }
+    //     fclose(file);
+    // }
+
+    //////// create socket and connect with client
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[4]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+            sizeof(serv_addr)) < 0)
+            error("ERROR on binding");
+    
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd,
+                (struct sockaddr *) &cli_addr,
+                &clilen); // newsockfd is the file descriptor for the accepted socket.
+    if (newsockfd < 0)
+        error("ERROR on accept");
+    
+    //////// write & read
+    while (1) {
+        int bytes_read = fread(buffer_ptr, 1, chunk_size, file);
+        printf("--- %d \n", bytes_read);
+        print_current_time_with_ms();
+        n = write(newsockfd, buffer_ptr, bytes_read);
+        if (n < 0) 
+            error("ERROR writing to socket");    
+
+        bzero(confirm_buffer,255);
+        n = read(newsockfd,confirm_buffer,255);
+        if (n < 0) 
+            error("ERROR reading from socket");
+
+        if ( bytes_read < chunk_size ){
+            n = write(newsockfd, "file transfer finished", 22);
+            if (n < 0) 
+                error("ERROR writing to socket"); 
+            printf("tcp_send() loop break\n");
+            break;
+        }
+    }
+
+    // bzero(buffer,256);
+    // n = read(newsockfd,buffer,1);
+    // if (n < 0) error("ERROR reading from socket");
+    // printf("Here is the message: %s\n",buffer);
+    // //print_current_time_with_ms();
+
+    close(newsockfd);
+    close(sockfd);
+
+    return 0;
+
+}
+
+int tcp_recv(int argc, char *argv[]){
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[10000]; // may not enough
+
+    portno = atoi(argv[4]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+    server = gethostbyname(argv[3]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+
+    while (1){
+        print_current_time_with_ms();
+        bzero(buffer,9999);
+        n = read(sockfd,buffer,9999);
+        if (n < 0) 
+            error("ERROR reading from socket");
+        printf("%s\n",buffer);
+
+        if (!strcmp("file transfer finished", buffer)) {
+            printf("tcp_recv() loop break\n");
+            break;
+        }
+
+        n = write(sockfd,"I got your message",18);
+        if (n < 0) 
+            error("ERROR writing to socket");
+    }
+
+    close(sockfd);
+    
+    return 0;
+}
+
+int udp_send(int argc, char *argv[]){
+    return 0;
+}
+
+int udp_recv(int argc, char *argv[]){
+    return 0;
+}
--- tcp/echo_once/server.c
+++ tcp/echo_once/server.c
@@ -8,6 +8,32 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define _POSIX_C_SOURCE 200809L
+
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+
+void print_current_time_with_ms (void)
+{
+    long            ms; // Milliseconds
+    time_t          s;  // Seconds
+    struct timespec spec;
+
+    clock_gettime(CLOCK_REALTIME, &spec);
+
+    s  = spec.tv_sec;
+    ms = round(spec.tv_nsec / 1.0e3); // Convert nanoseconds to milliseconds
+    if (ms > 999999) {
+        s++;
+        ms = 0;
+    }
+
+    printf("Current time: %"PRIdMAX".%03ld seconds since the Epoch\n",
+           (intmax_t)s, ms);
+}
+
 void error(const char *msg)
 {
     perror(msg);
@@ -16,49 +42,52 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
         error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd,
-                 (struct sockaddr *) &cli_addr,
-                 &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
 
-     
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message 2: %s\n",buffer);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message 3: %s\n",buffer);
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+            sizeof(serv_addr)) < 0)
+            error("ERROR on binding");
+            
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd,
+                (struct sockaddr *) &cli_addr,
+                &clilen);
+    // newsockfd is the file descriptor for the accepted socket.
+    if (newsockfd < 0)
+        error("ERROR on accept");
 
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0;
+    for(int i = 0; i < 3; ++i) {
+        printf("in server for\n");
+        
+        n = read(newsockfd,buffer,1);
+        if (n < 0) error("ERROR reading from socket");
+        printf("Here is the message: %s\n",buffer);
+        print_current_time_with_ms();
+        bzero(buffer,256);
+    }
+    // n = write(newsockfd,"I got your message",18);
+    // if (n < 0) error("ERROR writing to socket");
+
+    close(newsockfd);
+    close(sockfd);
+
+    return 0;
 }
