c706b73587e6ee964e972336127de2046a77dddc
Jui Chang <juichang@Juis-MacBook-Air.local>
2019-03-17 16:43:53 +0800

    remove some comment in udp functions, server send the file extension before sending file content

--- tcp/echo_once/lab1_file_transfer.c
+++ tcp/echo_once/lab1_file_transfer.c
@@ -24,8 +24,31 @@
         exit(EXIT_FAILURE); \
     } while (0)
 
-void print_current_time_with_ms (void)
-{
+
+void print_current_time_with_ms (void);
+void error(const char *msg);
+const char *get_filename_ext(const char *filename);
+
+int tcp_send(int argc, char *argv[]); // server
+int tcp_recv(int argc, char *argv[]); // client
+int udp_send(int argc, char *argv[]); // server
+int udp_recv(int argc, char *argv[]); // client
+
+int main(int argc, char *argv[]) {
+    if (!strcmp(argv[1], "tcp") && !strcmp(argv[2], "send")){
+        tcp_send(argc, argv);
+    } else if (!strcmp(argv[1], "tcp") && !strcmp(argv[2], "recv")){
+        tcp_recv(argc, argv);
+    } else if (!strcmp(argv[1], "udp") && !strcmp(argv[2], "send")){
+        udp_send(argc, argv);
+    } else if (!strcmp(argv[1], "udp") && !strcmp(argv[2], "recv")){
+        udp_recv(argc, argv);
+    } else
+        printf("ERROR : invalid arguments\n");
+    return 0;
+}
+
+void print_current_time_with_ms (void) {
     long            ms; // Microseconds
     time_t          s;  // Seconds
     struct timespec spec;
@@ -43,33 +66,18 @@ void print_current_time_with_ms (void)
            (intmax_t)s, ms);
 }
 
-void error(const char *msg)
-{
+void error(const char *msg) {
     perror(msg);
     exit(1);
 }
 
-int tcp_send(int argc, char *argv[]); // server
-int tcp_recv(int argc, char *argv[]); // client
-int udp_send(int argc, char *argv[]);
-int udp_recv(int argc, char *argv[]);
-
-int main(int argc, char *argv[]){
-    if (!strcmp(argv[1], "tcp") && !strcmp(argv[2], "send")){
-        printf("main() if : tcp_send\n");
-        tcp_send(argc, argv);
-    } else if (!strcmp(argv[1], "tcp") && !strcmp(argv[2], "recv")){
-        tcp_recv(argc, argv);
-    } else if (!strcmp(argv[1], "udp") && !strcmp(argv[2], "send")){
-        udp_send(argc, argv);
-    } else if (!strcmp(argv[1], "udp") && !strcmp(argv[2], "recv")){
-        udp_recv(argc, argv);
-    } else
-        printf("ERROR : arguments invalid\n");
-    return 0;
+const char *get_filename_ext(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return "";
+    return dot + 1;
 }
 
-int tcp_send(int argc, char *argv[]){
+int tcp_send(int argc, char *argv[]) {
 
     int sockfd, newsockfd, portno;
     socklen_t clilen;
@@ -80,6 +88,7 @@ int tcp_send(int argc, char *argv[]){
     int chunk_size;
     int write_size;
     char *buffer_ptr;
+    char file_ext[10];
     FILE *file;
     size_t nread;
 
@@ -119,6 +128,13 @@ int tcp_send(int argc, char *argv[]){
         error("ERROR on accept");
     
     //////// write & read
+
+    // send file extension
+    bzero(file_ext, sizeof(file_ext));
+    strcpy(file_ext, get_filename_ext(argv[5]));  
+    n = write(newsockfd, &file_ext, sizeof(file_ext));
+    if (n < 0) error("ERROR writing to socket");
+
     while (1) {
         bzero(buffer_ptr, chunk_size);
         int bytes_read = fread(buffer_ptr, 1, chunk_size, file);
@@ -157,10 +173,15 @@ int tcp_recv(int argc, char *argv[]){
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[100000]; // may not enough
     int read_size;
-    int percent = 0;
+    char buffer[100000]; // may not enough
+    char file_ext[10];
+    char file_name[50];
     FILE *file;
+
+    int percent = 0;
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
 
     portno = atoi(argv[4]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -182,14 +203,18 @@ int tcp_recv(int argc, char *argv[]){
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
 
-    file = fopen("receiver.jpg","w");
+    // recv file extension
+    bzero(file_ext,sizeof(file_ext));
+    n = read(sockfd, &file_ext, sizeof(file_ext));
+    if (n < 0) error("ERROR reading from socket");
+    strcpy(file_name, "receiver.");
+    strcat(file_name, file_ext);
+
+    file = fopen(file_name,"w");
     if (file == NULL){
         printf("Unable to create file.\n");
         exit(EXIT_FAILURE);
     }
-    
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
 
     while (1){
 
@@ -229,16 +254,17 @@ int udp_send(int argc, char *argv[]){
     printf("udp_send()\n");
     int sockfd, portno;
     socklen_t clilen;
-    char confirm_buffer[256];
     struct sockaddr_in serv_addr;
     struct sockaddr_in peeraddr;
     socklen_t peerlen;
-    char recvbuf[1024] = {0};
     int n;
 
     int chunk_size;
+    char recvbuf[1024] = {0};
+    char file_ext[10];
     int write_size;
     char *buffer_ptr;
+    char confirm_buffer[256];
     FILE *file;
     size_t nread;
 
@@ -271,11 +297,17 @@ int udp_send(int argc, char *argv[]){
     
     //////// write & read
 
+    // recv call from client
     peerlen = sizeof(peeraddr);
     memset(recvbuf, 0, sizeof(recvbuf));
     n = recvfrom(sockfd, recvbuf, sizeof(recvbuf), 0, (struct sockaddr *)&peeraddr, &peerlen);
     if (n == -1 && errno != EINTR)
         ERR_EXIT("recvfrom error");
+
+    // send file extension
+    bzero(file_ext, sizeof(file_ext));
+    strcpy(file_ext, get_filename_ext(argv[5]));
+    sendto(sockfd, &file_ext, sizeof(file_ext), 0, (struct sockaddr *)&peeraddr, peerlen);
     
     while (1) {
         bzero(buffer_ptr, chunk_size);
@@ -285,10 +317,7 @@ int udp_send(int argc, char *argv[]){
             write_size = 22;
             sendto(sockfd, &write_size, sizeof(write_size), 0, (struct sockaddr *)&peeraddr, peerlen);
             sendto(sockfd, "file transfer finished", 22, 0, (struct sockaddr *)&peeraddr, peerlen);
-            // n = write(newsockfd, &write_size, sizeof(write_size));
-            // if (n < 0) error("ERROR writing to socket"); 
-            // n = write(newsockfd, "file transfer finished", 22);
-            // if (n < 0) error("ERROR writing to socket"); 
+            
             printf("Sender: File transfer finished.\n");
             break;
         }
@@ -296,18 +325,12 @@ int udp_send(int argc, char *argv[]){
 
         write_size = bytes_read;
         sendto(sockfd, &write_size, sizeof(write_size), 0, (struct sockaddr *)&peeraddr, peerlen);
-        sendto(sockfd, buffer_ptr, bytes_read, 0, (struct sockaddr *)&peeraddr, peerlen);
-        // n = write(newsockfd, &write_size, sizeof(write_size));
-        // if (n < 0) error("ERROR writing to socket"); 
-        // n = write(newsockfd, buffer_ptr, bytes_read);
-        // if (n < 0) error("ERROR writing to socket");    
+        sendto(sockfd, buffer_ptr, bytes_read, 0, (struct sockaddr *)&peeraddr, peerlen);  
 
         bzero(confirm_buffer,255);
         n = recvfrom(sockfd, confirm_buffer, sizeof(confirm_buffer), 0, (struct sockaddr *)&peeraddr, &peerlen);
         if (n == -1 && errno != EINTR)
             ERR_EXIT("recvfrom error");    
-        // n = read(newsockfd,confirm_buffer,255);
-        // if (n < 0) error("ERROR reading from socket");
     }
 
     // close(newsockfd);
@@ -322,12 +345,18 @@ int udp_recv(int argc, char *argv[]){
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    char sendbuf[1024] = {0};
 
-    char buffer[100000]; // may not enough
+    char sendbuf[1024] = {0};
+    char file_name[50];
+    char file_ext[10];
     int read_size;
+    char buffer[100000]; // may not enough
+
     int percent = 0;
     FILE *file;
+
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
 
     portno = atoi(argv[4]);
     sockfd = socket(PF_INET, SOCK_DGRAM, 0);
@@ -343,46 +372,36 @@ int udp_recv(int argc, char *argv[]){
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(portno);
-    //servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     bcopy((char *)server->h_addr, 
          (char *)&servaddr.sin_addr.s_addr,
          server->h_length);
-    // bzero((char *) &serv_addr, sizeof(serv_addr));
-    // serv_addr.sin_family = AF_INET;
-    // bcopy((char *)server->h_addr, 
-    //      (char *)&serv_addr.sin_addr.s_addr,
-    //      server->h_length);
-    // serv_addr.sin_port = htons(portno);
-    // if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-    //     error("ERROR connecting");
 
-    file = fopen("receiver.jpg","w");
+    ////// write and read
+
+    // call the server
+    sendto(sockfd, sendbuf, strlen(sendbuf), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    // recv the file extension
+    bzero(file_ext,sizeof(file_ext));
+    n = recvfrom(sockfd, &file_ext, sizeof(file_ext), 0, NULL, NULL);
+    if (n == -1 && errno != EINTR) ERR_EXIT("recvfrom");
+    strcpy(file_name, "receiver.");
+    strcat(file_name, file_ext);
+
+    file = fopen(file_name,"w");
     if (file == NULL){
         printf("Unable to create file.\n");
         exit(EXIT_FAILURE);
     }
-    
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-
-    sendto(sockfd, sendbuf, strlen(sendbuf), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
     while (1){
         n = recvfrom(sockfd, &read_size, sizeof(read_size), 0, NULL, NULL);
         if (n == -1 && errno != EINTR) ERR_EXIT("recvfrom");
-        // n = read(sockfd, &read_size, sizeof(read_size));
-        // if (n < 0) error("ERROR reading from socket");
         bzero(buffer,99999);
         n = recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL);
         if (n == -1 && errno != EINTR) ERR_EXIT("recvfrom");
-        // n = read(sockfd, buffer, 99999);
-        // if (n < 0) error("ERROR reading from socket");
-        //printf("%s\n",buffer);
         if (strcmp("file transfer finished", buffer)) {
-            //printf("%lu\n", strlen(buffer));
-            //fputs(buffer, file);
             fwrite(buffer, 1, read_size, file);
-            //printf("buffer : %lu\n", sizeof(buffer));
             percent += 5;               // yet : should send the offset first
             if (percent != 100) {
                 if (percent == 105)
@@ -396,8 +415,6 @@ int udp_recv(int argc, char *argv[]){
             break;
         }
         sendto(sockfd, "I got your message",18, 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
-        // n = write(sockfd,"I got your message",18);
-        // if (n < 0) error("ERROR writing to socket");
     }
 
     close(sockfd);
